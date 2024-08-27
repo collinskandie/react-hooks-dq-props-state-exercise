0002ec3f1c3eadda8f3cff2d4fc908e4c21cc879
Collins Kandie <collinskandie1@gmail.com>
2024-08-27 20:04:07 +0300

    working

--- src/components/HogBoss.js
+++ src/components/HogBoss.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Boss from "../assets/boss-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+import offspring from "../data.js"; // Importing baby data
 
 function HogBoss() {
   const [eyeColor, setEyeColor] = useState("blue");
@@ -18,31 +18,36 @@ function HogBoss() {
         value="blue"
         onChange={handleChangeEyeColor}
       />
-      Blue<br></br>
+      Blue<br />
       <input
         type="radio"
         name="eyeColor"
         value="sun"
         onChange={handleChangeEyeColor}
       />
-      Sun<br></br>
+      Sun<br />
       <input
         type="radio"
         name="eyeColor"
         value="glowing"
         onChange={handleChangeEyeColor}
       />
-      Glowing<br></br>
+      Glowing<br />
       <h2>Name: Blaster Boss</h2>
       <h3>Weight: 2.54 Tons</h3>
       <h3>Eye Color: {eyeColor}</h3>
       <div id="boss-domicile">
-        <img id="boss-blaster" src={Boss} alt="" />
+        <img id="boss-blaster" src={Boss} alt="Boss Hog" />
       </div>
       <ul className="hoglist">
-        <BabyHog />
-        <BabyHog />
-        <BabyHog />
+        {offspring.map((baby) => (
+          <BabyHog
+            key={baby.id}
+            name={baby.name}
+            hobby={baby.hobby}
+            eyeColor={eyeColor}
+          />
+        ))}
       </ul>
     </div>
   );
